6d2c2f942be4d3f0c8def4b5c1520fd85e4bc4e8
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-03-09 13:22:00 -0500

    test

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -5,4 +5,13 @@ defmodule ListLengthTest do
   test "greets the world" do
     assert ListLength.hello() == :world
   end
+
+  describe "call/1" do
+    test "returns the length of a list" do
+      list = [1, 2, 3, 4]
+      response = ListLength.call(list)
+      expected_response = 4
+      assert response == expected_response
+    end
+  end
 end
